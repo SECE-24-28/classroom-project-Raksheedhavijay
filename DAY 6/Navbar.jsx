881ae43cc4658591ab71d05e8b1ddab0ebf07a9c
Raksheedhavijay <raksheedhavk@gmail.com>
2025-12-10 15:55:53 +0530

    Add files via upload

---
+++ DAY 6/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <p>The above content is Navbar</p>
+    </>
+  )
+}
+export default Navbar
---
+++ DAY 6/index.js
@@ -0,0 +1,1 @@
+console.log("Hello Raksheedha! Node.js is running ");
